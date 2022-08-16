1ad367dcc59366349840cf14bffa38d120c1fc3c
Harsh Jain <jainharsh8506@gmail.com>
2022-08-16 21:50:22 +0530

    made pages

--- Hello/NWNS/urls.py
+++ Hello/NWNS/urls.py
@@ -4,6 +4,9 @@ from NWNS import views
 
 urlpatterns = [
     path("", views.index, name='home'),
-    path("about", views.about, name="about")
+    path("about", views.about, name="about"),
+    path("university", views.university, name="university"),
+    path("standalone", views.standalone, name="standalone"),
+    path("college_institution", views.college_institution, name="college_institution")
     
 ]
--- Hello/NWNS/views.py
+++ Hello/NWNS/views.py
@@ -1,5 +1,7 @@
 from http.client import HTTPResponse
 from django.shortcuts import render, HttpResponse
+from .models import RefInstituteType , College
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -7,5 +9,15 @@ def index(request):
     return render(request,'index.html')
 
 def about(request):
-    return HttpResponse("this is about page")
+    return render(request,'about.html')
+
+def university(request):
+    return render(request,'university.html')
+
+def standalone(request):
+    return render(request,'standalone.html')
+
+def college_institution(request):
+    return render(request,'college_institution.html')
+
 
