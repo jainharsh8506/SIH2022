3434fc2af8778d6dfcac2771ee015f0384f548de
Harsh Jain <jainharsh8506@gmail.com>
2022-08-16 13:35:42 +0530

    .

--- Hello/NWNS/views.py
+++ Hello/NWNS/views.py
@@ -4,10 +4,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    context={
-        'variable':" this is sent"
-    }
-    return render(request,'index.html',context)
+    return render(request,'index.html')
 
 def about(request):
     return HttpResponse("this is about page")
