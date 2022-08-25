75c0ffd5eb2fb81088aa60836c7d808cc40d22b1
Harsh Jain <jainharsh8506@gmail.com>
2022-08-25 17:34:18 +0530

    hvgh

--- Hello/NWNS/views.py
+++ Hello/NWNS/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, HttpResponse
 from .models import CollegeInstitution, CollegeInstitutionAccreditation, RefInstituteType, StandaloneInstitution, University
 from django.http import JsonResponse
 import pandas as pd
+import json
 from neww import hdd
 # Create your views here.
 
@@ -63,7 +64,23 @@ def accreditation_standalone_institution(request):
     return render(request,'accreditation_standalone_institution.html')
 
 def accreditation_university(request):
-    return render(request,'accreditation_university.html')
+    top10univacc2019=pd.read_csv("static/csv/top_10_accreditation_2019.csv")
+    json_records = top10univacc2019.reset_index().to_json(orient ='records')
+    univacc2019 = []
+    univacc2019 = json.loads(json_records)
+    top10univacc2018=pd.read_csv("static/csv/top_10_accreditation_2018.csv")
+    json_records = top10univacc2018.reset_index().to_json(orient ='records')
+    univacc2018 = []
+    univacc2018 = json.loads(json_records)
+    
+    top10univacc2017=pd.read_csv("static/csv/top_10_accreditation_2017.csv")
+    json_records = top10univacc2017.reset_index().to_json(orient ='records')
+    univacc2017 = []
+    univacc2017 = json.loads(json_records)
+    
+    year_type= request.GET.get('year_type')
+    context={'univacc2019':univacc2019,'univacc2018':univacc2018,'univacc2017':univacc2017,'year_type':year_type}
+    return render(request,'accreditation_university.html',context)
 
 def infrastructure_college_institution(request):
     return render(request,'infrastructure_college_institution.html')
